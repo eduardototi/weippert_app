280aa96cbcb4c881f027b56441720a65b6a97388
Pedro Eduardo <eduardototi@gmail.com>
2021-05-03 10:49:38 -0300

    set house rented

--- app/models/contract.rb
+++ app/models/contract.rb
@@ -1,4 +1,26 @@
 class Contract < ApplicationRecord
   belongs_to :house
   belongs_to :locator
+
+  before_save :set_contract_expiration_date, :set_total_amount, :set_contract_active
+  after_save :set_house_rented 
+
+  private
+
+  def set_contract_expiration_date
+    self.contract_expiration_date = Date.today + (self.contract_term * 30)
+  end
+
+  def set_total_amount
+    self.iptu_charge ? self.total_amount = self.reantal_amount + self.iptu_amount : self.total_amount = self.reantal_amount
+  end
+
+  def set_contract_active
+    self.is_active = true
+  end
+
+  def set_house_rented
+    self.house.rental_status = true
+    self.house.save
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -9,4 +9,6 @@ Rails.application.routes.draw do
 
   resources :locators
 
+  resources :contracts
+
 end
